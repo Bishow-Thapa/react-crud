af8d10ae683912d277263171efcafdd03dc888fe
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-02 13:25:03 +0545

    Part 2: Insert, Select completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([]);
+
+  useEffect(()=> {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
+      setMovieList(response.data)
+    });
+  }, []);
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      movieName: movieName,
+      review: review
+    }).then(() => {
+      alert('successfull insert');
+    });
+  }
+
   return (
     <div className="App">
       <h4 className="title">CRUD APPLICATION</h4>
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" />
+        <input 
+          type="text"
+          name="movieName"
+          onChange={(e)=> {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Movie Review:</label>
-        <textarea rows="" columns="" name="movieReview"></textarea>
-        <button>Submit</button>
+        <textarea 
+          rows=""
+          columns=""
+          name="movieReview"
+          onChange={(e)=> {
+            setReview(e.target.value);
+        }}></textarea>
+        <button onClick={submitReview}>Submit</button>
+
+        {movieReviewList.map((val)=> {
+          return <h1>MovieName: {val.movieName} | Movie review: {val.movieReview}</h1>
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+// database
+const mysql = require('mysql');
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'reactdb',
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/get', (req, res)=> {
+    const sqlSelect = "SELECT * FROM movie_detail"
+    db.query(sqlSelect, (err, result)=> {
+        res.send(result);
+    });
+})
+
+app.post('/api/insert', (req, res) => {
+    const movieName = req.body.movieName
+    const movieReview = req.body.review
+
+    const sqlInsert = "INSERT INTO movie_detail (movieName, movieReview) VALUES (?, ?);"
+    db.query(sqlInsert, [movieName, movieReview], (err, result)=> {
+        console.log(result);
+    });
+})
 
 app.listen(3001, ()=> {
     console.log("running on port 3001");

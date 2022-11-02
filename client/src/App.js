54484bdb9edfbe26f3e7831f9b594fba50842be4
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-02 09:41:16 +0545

    added server

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4 className="title">CRUD APPLICATION</h4>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName" />
+        <label>Movie Review:</label>
+        <textarea rows="" columns="" name="movieReview"></textarea>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
